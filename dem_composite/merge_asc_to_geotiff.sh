b73c1cab5448326865e38318aa48056e2a756787
Ian Dees <ian.dees@gmail.com>
2017-07-07 18:09:42 -0400

    Quiet things down a little

--- dem_composite/merge_asc_to_geotiff.sh
+++ dem_composite/merge_asc_to_geotiff.sh
@@ -4,9 +4,10 @@
 set -e
 
 zfile=$1
+ofile=$2
 
-if [[ -z "$zfile" ]]; then
-  >&2 echo "usage: $(basename $0) <input zipfile>"
+if [[ -z "$zfile" || -z "$ofile" ]]; then
+  >&2 echo "usage: $(basename $0) <input zipfile name> <output geotiff name>"
   exit 1
 fi
 
@@ -15,9 +16,18 @@ vrt_tmp=$(mktemp -d)
 unzip -lqq $zfile \
  | awk '{print $4}' \
  | while read filename; do
-     gdalwarp -s_srs "EPSG:27700" -t_srs "EPSG:27700" -of VRT /vsizip/$zfile/$filename $vrt_tmp/${filename/asc/vrt}
+     gdalwarp -q -s_srs "EPSG:27700" -t_srs "EPSG:27700" -of VRT /vsizip/$zfile/$filename $vrt_tmp/${filename/asc/vrt}
    done
 
-gdal_merge.py -a_nodata -9999 -init -9999 -o ${zfile/zip/tif} $vrt_tmp/*.vrt
+gdal_merge.py \
+    -q \
+    -co COMPRESS=DEFLATE \
+    -co PREDICTOR=3 \
+    -co TILED=yes \
+    -co BLOCKXSIZE=512 \
+    -co BLOCKYSIZE=512 \
+    -a_nodata -9999 \
+    -init -9999 \
+    -o $ofile $vrt_tmp/*.vrt
 
 rm -rf $vrt_tmp
